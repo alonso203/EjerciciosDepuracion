b8403abbb118f36aa706d19a2e8f63d920873ae4
alonso203 <jfq72062@educastur.es>
2025-10-16 17:43:18 +0200

    modificacion pedirNumero

--- src/ejercicios/depuracion/Ejemplo2While.java
+++ src/ejercicios/depuracion/Ejemplo2While.java
@@ -5,14 +5,18 @@ package ejercicios.depuracion;
  */
 import java.util.*;
 public class Ejemplo2While {
-    public static void main(String[] args) {
+    public static int pedirNumero() {
         Scanner sc = new Scanner(System.in);
-        int n, contador = 0;
         System.out.print("Introduce un número: ");
-        n = sc.nextInt();
+        return sc.nextInt();
+    }
+    public static void main(String[] args) {
+        int n, contador = 0;
+        n = pedirNumero();
         while (contador< n){
                System.out.println(" * ");
                contador++;
+               System.out.println("Contador: " + contador);
         }
     }
 }
